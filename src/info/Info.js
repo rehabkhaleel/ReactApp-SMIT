aa634942dbd8f10b7fc0fd6e8e12ada0b38089ae
Rehab <140162802+rehabkhaleel@users.noreply.github.com>
2024-05-05 10:29:33 +0500

    Add files via upload

---
+++ src/info/Info.js
@@ -0,0 +1,110 @@
+import self from "../img/self.png"
+import mock1 from "../img/mock1.png"
+import mock2 from "../img/mock2.png"
+import mock3 from "../img/mock3.png"
+import mock4 from "../img/mock4.png"
+import mock5 from "../img/mock5.png"
+
+
+export let colors = ["rgb(0,255,164)", "rgb(166,104,255)"];
+
+export let singlePage = false;
+
+export const info = {
+    firstName: "Rehab",
+    lastName: "Khaleel",
+    initials: "rk", 
+    position: "a Full Stack Developer",
+    selfPortrait: self, 
+    gradient: `-webkit-linear-gradient(135deg, ${colors})`, 
+    baseColor: colors[0],
+    miniBio: [
+        {
+            emoji: '☕',
+            text: 'fueled by coffee'
+        },
+        {
+            emoji: '🌎',
+            text: 'based in the Pakistan'
+        },
+        {
+            emoji: "💼",
+            text: "Undergraduate Software Engineer"
+        },
+        {
+            emoji: "📧",
+            text: "rehabkhaleel716@gmail.com"
+        }
+    ],
+    socials: [
+        {
+            link: "https://instagram.com",
+            icon: 'fa fa-instagram',
+            label: 'instagram'
+        },
+        {
+            link: "https://github.com/rehabkhaleel",
+            icon: "fa fa-github",
+            label: 'github'
+        },
+        {
+            link: "https://www.linkedin.com/in/rehab-khaleel/",
+            icon: "fa fa-linkedin",
+            label: 'linkedin'
+        },
+        {
+            link: "https://twitter.com/KhaleelRehab",
+            icon: "fa fa-twitter",
+            label: 'twitter'
+        }
+    ],
+    bio: "Hey. I'm a full stack developer. I am currently studying Software Engineering at Bahria University.",
+    skills:
+        {
+            proficientWith: ['javascript', 'react', 'git', 'github', 'bootstrap', 'html5', 'css3', 'c#','c','python','java','bash'],
+            exposedTo: ['nodejs', 'c++', 'figma']
+        }
+    ,
+    hobbies: [
+        {
+            label: 'reading',
+            emoji: '📖'
+        },
+        {
+            label: 'theater',
+            emoji: '🎭'
+        }
+    ],
+    portfolio: [ 
+        {
+            title: "Project 1",
+            live: "https://paytonpierce.dev", 
+            source: "https://github.com/paytonjewell", 
+            image: mock1
+        },
+        {
+            title: "Project 2",
+            live: "https://paytonpierce.dev",
+            source: "https://github.com/paytonjewell",
+            image: mock2
+        },
+        {
+            title: "Project 3",
+            live: "https://paytonpierce.dev",
+            source: "https://github.com/paytonjewell",
+            image: mock3
+        },
+        {
+            title: "Project 4",
+            live: "https://paytonpierce.dev",
+            source: "https://github.com/paytonjewell",
+            image: mock4
+        },
+        {
+            title: "Project 5",
+            live: "https://paytonpierce.dev",
+            source: "https://github.com/paytonjewell",
+            image: mock5
+        }
+    ]
+}
